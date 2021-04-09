1c9f88691a3a767f8adbe0127e53c1e085e3e0fd
Lorenzo <lorenzo.antonelli89@gmail.com>
2021-04-09 15:01:36 +0200

    update messaggi

--- js/main.js
+++ js/main.js
@@ -7,74 +7,172 @@ function initVue() {
                 { 
                     img: 'img/boolean.png', 
                     name: 'Boolean#29', 
-                    message: 'ciaooo', 
+                    message: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            text: 'Lo sai che ha aperto una nuova pizzeria?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            text: 'Si, ma preferirei andare al cinema',
+                            status: 'received'
+                        }
+                    ],
                     date: 'Yesterday'
-                },
-                { 
-                    img: 'img/baffi.png', 
-                    name: 'PizziBoys', 
-                    message: 'Arrivo!', 
-                    date: 'Yesterday' 
                 },
                 { 
                     img: 'img/goblin.png', 
                     name: 'Jumbotron', 
-                    message: 'Ci sono!', 
+                    message: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            text: 'Lo sai che ha aperto una nuova birreria?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            text: 'Si, ma preferirei andare al parco',
+                            status: 'received'
+                        }
+                    ],
                     date: 'Monday' 
                 },
                 { 
                     img: 'img/apple.png', 
                     name: 'Martino', 
-                    message: 'perche tiziana preferisce te?', 
+                    message: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            text: 'Dobbiamo finire il compito!',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            text: 'lo finirò domani, devo fare minesweep',
+                            status: 'received'
+                        }
+                    ],
                     date: 'Monday' 
                 },
                 { 
                     img: 'img/random1.png', 
                     name: 'Tiziana', 
-                    message: 'sei meglio di martino', 
+                    message: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            text: 'Lo sai che ha aperto una nuova pizzeria?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            text: 'Si, ma preferirei andare al parco',
+                            status: 'received'
+                        }
+                    ],
                     date: 'Monday' 
                 },
                 { 
                     img: 'img/persona.png', 
                     name: 'Olga', 
-                    message: 'esercizio corretto, bravo!', 
+                    message: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            text: 'Andava bene il mio esercizio?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            text: 'si andava bene',
+                            status: 'received'
+                        }
+                    ],
                     date: 'Sunday' 
                 },
                 { 
                     img: 'img/cyberpunk.png', 
                     name: 'Gianluca', 
-                    message: 'Andiamo a giocare a padel?', 
+                    message: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            text: 'se ti va ti insegno a giocare',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            text: 'andiamo a giocare a padel?',
+                            status: 'received'
+                        }
+                    ],
                     date: 'Sunday' 
                 },
                 { 
                     img: 'img/random.png', 
                     name: 'Beppe', 
-                    message: 'ti richiamo dopo', 
+                    message: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            text: 'Lo sai che ha aperto una nuova pizzeria?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            text: 'Si, ma preferirei andare al cinema',
+                            status: 'received'
+                        }
+                    ],
                     date: 'Sunday' 
                 },
                 { 
                     img: 'img/fb.png', 
                     name: 'Mark', 
-                    message: 'ti richiamo dopo', 
+                    message: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            text: 'Lo sai che ha aperto una nuova pizzeria?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            text: 'Si, ma preferirei andare al cinema',
+                            status: 'received'
+                        }
+                    ],
                     date: 'Sunday' 
                 },
                 { 
                     img: 'img/random2.png', 
                     name: 'Francesco', 
-                    message: 'ti richiamo dopo', 
+                    message: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            text: 'Lo sai che ha aperto una nuova pizzeria?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            text: 'Si, ma preferirei andare al cinema',
+                            status: 'received'
+                        }
+                    ],
                     date: 'Saturday' 
                 },
                 { 
-                    img: 'img/random3.png', 
+                    img: 'img/among.png', 
                     name: 'Alessandro', 
-                    message: 'ti richiamo dopo', 
+                    message: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            text: 'Lo sai che ha aperto una nuova pizzeria?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            text: 'Si, ma preferirei andare al cinema',
+                            status: 'received'
+                        }
+                    ],
                     date: 'Saturday' 
-                },
-                {
-                    img: 'img/among.png',
-                    name: 'Ale Dongu',
-                    message: 'ti richiamo dopo',
-                    date: 'Saturday'
                 },
 
             ],
@@ -93,26 +191,26 @@ function initVue() {
         },
         computed: {
             // funzione per filtrare nomi lista e rendere visibili solo quelli che iniziano con la lattere che scrivi nel search
-            // filteredContacts() {
-            //     return this.contacts.filter(elem => {
-            //         return elem.name.toLowerCase().includes(this.searchName.toLowerCase());
-            //     });
-            // }
             filteredContacts() {
-
-                let filtered = [];
-
-                for(let i = 0; i < this.contacts.length; i++){
-                    // console.log(this.contacts[i]['name']);
-                    let name = this.contacts[i]['name'];
-                    if (name.includes(this.searchName)) {
-                        filtered.push(name)
-                    }else{
-                        return this.contacts;
-                    }
-                }
-                console.log(filtered);
+                return this.contacts.filter(elem => {
+                    return elem.name.toLowerCase().includes(this.searchName.toLowerCase());
+                });
             }
+            // filteredContacts() {
+
+            //     let filtered = [];
+
+            //     for(let i = 0; i < this.contacts.length; i++){
+            //         // console.log(this.contacts[i]['name']);
+            //         let name = this.contacts[i]['name'];
+            //         if (name.includes(this.searchName)) {
+            //             filtered.push(name)
+            //         }else{
+            //             return this.contacts;
+            //         }
+            //     }
+            //     console.log(filtered);
+            // }
         }
     });
 }
